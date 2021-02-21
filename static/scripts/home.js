afcf46e5bdded0c779b68e6baeea7c5de212beb8
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-02-21 10:51:08 +0000

    Added script to hide dominant header elements when a main image is visible

---
+++ static/scripts/home.js
@@ -0,0 +1,42 @@
+const mainImages = document.getElementsByClassName('main-img');
+
+window.onload = () => {
+    stateChecker.shouldHideHeader();
+}
+
+window.onscroll = () => {
+    stateChecker.shouldHideHeader();
+}
+
+stateChecker = {
+    shouldHideHeader: () => {
+        if(mainImages) { //if there is a dominant image in the page
+            for (let i = 0; i < mainImages.length; i++) {
+                if (scrollEvents.isOnScreen(mainImages[i])) { //hide the dominant navbar elements if there is a main background image visible
+                    $('#logo').css({
+                        'opacity': 0
+                    });
+                    $('#page-header').css({
+                        'background-color': 'var(--transparent)'
+                    });
+                }
+                else {
+                    $('#logo').css({
+                        'opacity': 100
+                    });
+                    $('#page-header').css({
+                        'background-color': 'var(--background-colour)'
+                    });
+                }
+            }
+        }
+    }
+}
+
+scrollEvents = {
+    isOnScreen: (element) => { //used to determine if an HTML element is on screen - https://stackoverflow.com/a/5354536/11136104
+		var rect = element.getBoundingClientRect();
+		var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+		return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+	}
+}
