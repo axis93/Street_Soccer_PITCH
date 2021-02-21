ab7d4937c6853ed3a920eeaa2da803dbbc65475a
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-02-21 17:05:18 +0000

    Added mobile navigation menu

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -14,7 +14,7 @@ stateChecker = {
     shouldHideHeader: () => {
         if(mainImages) { //we need to check this here also as 'window.onload()' will run this method separately
             for (let i = 0; i < mainImages.length; i++) {
-                if (scrollEvents.isOnScreen(mainImages[i])) { //hide the dominant navbar elements if there is a main background image visible
+                if (events.isOnScreen(mainImages[i])) { //hide the dominant navbar elements if there is a main background image visible
                     $('#logo').css({
                         'opacity': 0
                     });
@@ -35,10 +35,16 @@ stateChecker = {
     }
 }
 
-scrollEvents = {
+events = {
     isOnScreen: (element) => { //used to determine if an HTML element is on screen - https://stackoverflow.com/a/5354536/11136104
 		var rect = element.getBoundingClientRect();
 		var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
 		return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
-	}
+	},
+
+    navOperationClicked: (operation) => {
+        $('#navigation').css({
+            'animation': `${operation}-mobile-nav 0.5s ease forwards`
+        });
+    }
 }
