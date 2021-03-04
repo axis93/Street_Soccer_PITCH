454eb2f023efceef77efcf7ef307baec54868751
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-03-04 19:48:30 +0000

    Can now query answers along with questions

--- resources/quiz.py
+++ resources/quiz.py
@@ -4,10 +4,10 @@ from models.quiz import QuizModel
 class Quiz(Resource):
     def get(self, quiz_id):
         try:
-            topic = QuizModel.find_by_id(quiz_id)
+            quiz = QuizModel.find_by_id(quiz_id)
         except:
             return {'message': 'An error occurred while reading the quiz ID from the database'}, 500
         
-        if topic:
-            return topic.json()
+        if quiz:
+            return quiz.json()
         return {'message': 'Quiz with the ID {} not found'.format(quiz_id)}, 404
