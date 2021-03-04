9c3b331f471ebdd08a7c91289317e57ea98eeb97
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-03-04 16:43:31 +0000

    Re-enabled missing ID exception

--- resources/test.py
+++ resources/test.py
@@ -3,10 +3,10 @@ from models.test import TestModel
 
 class Test(Resource):
     def get(self, test_id):
-        #try:
-        topic = TestModel.find_by_id(test_id)
-        #except:
-        #    return {'message': 'An error occurred while reading the test ID from the database'}, 500
+        try:
+            topic = TestModel.find_by_id(test_id)
+        except:
+            return {'message': 'An error occurred while reading the test ID from the database'}, 500
         
         if topic:
             return topic.json()
