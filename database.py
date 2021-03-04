7d0f6b77e001e231e74ca1a583f99dc5b3577893
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-03-04 16:14:43 +0000

    Added an internal comment

--- database.py
+++ database.py
@@ -1,5 +1,25 @@
 from app import init_db
 
+"""
+this would prevent having to duplicate the 'query_db'
+method in every Model
+
+this can't be used because we need to import this in the
+models, but the models are imported in 'app.py', so this
+becomes a circular dependancy
+
+def query_db(self, query, args):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+
+    result = cursor.execute(query, args)
+    row = result.fetchone()
+
+    connection.close()
+
+    return row
+"""
+
 insert_queries = (
     "INSERT INTO topics (topic_id, is_unlocked, name, needed_credit) VALUES (1, 1, 'Child Protection Policy', 60);",
     "INSERT INTO tests (test_id, topic_id, is_unlocked, max_credit, order_num, gained_credit, pass_credit, description, is_retakable) VALUES (1, 1, 1, 80, 5, 0, 60, 'For this test you will need to complete two quizzes, please pres the button to carry on', 1);",
