a7d44e60dd191f6cc61e120960e032f4c2fb7b55
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-03-19 07:54:34 +0000

    Removed commented code

--- app.py
+++ app.py
@@ -63,29 +63,6 @@ api.add_resource(Test, '/tests')
 api.add_resource(Quiz, '/quizzes')
 api.add_resource(Answer, '/answers')
 
-"""
-use Ctrl+F5 to clear the cache and refresh
-
-caching stops the static files from being refreshed when the page
-is refreshed
-
-this was an attempt to clear the browser cache for this page
-automatically so we don't have to do it manually everytime we test
-changes on a page
-
-@app.context_processor
-def override_url_for():
-	return dict(url_for=dated_url_for)
-
-def dated_url_for(endpoint, **values):
-	if endpoint == 'static':
-		filename = values.get('filename', None)
-		if filename:
-			file_path = os.path.join(app.root_path, endpoint, filename)
-			values['q'] = int(os.stat(file_path).st_mtime)
-	return url_for(endpoint, **values)
-"""
-
 if __name__ == "__main__":
 	database.init_app(app)
 	app.run(host='127.0.0.1', debug=True)
